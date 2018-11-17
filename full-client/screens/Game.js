1e711fbcd1091cf30aa5c114587eca96976dd091
Aibek Smagulov <smagulovaybek@gmail.com>
2018-11-18 01:01:08 +0900

    client sending a number of characters correctly inputted

--- full-client/screens/Game.js
+++ full-client/screens/Game.js
@@ -3,20 +3,24 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 import firebase from 'firebase'
 import io from 'socket.io-client'
 
+let socket
+
 export default class Game extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       text: 'Loading..',
-      input: ''
+      input: '',
+      chars: 0
     }
     this.setSocketBehavior = this.setSocketBehavior.bind(this)
     this.handleSignOut = this.handleSignOut.bind(this)
     this.handleUserInput = this.handleUserInput.bind(this)
+    this.sendRaceData = this.sendRaceData.bind(this)
   }
 
   setSocketBehavior (idToken) {
-    const socket = io.connect('http://10.64.128.209:3000', { reconnect: true })
+    socket = io.connect('http://10.64.128.209:3000', { reconnect: true })
     socket.on('connect', () => {
       socket.emit('authentication', { token: idToken })
       socket.on('authenticated', () => {
@@ -26,7 +30,13 @@ export default class Game extends React.Component {
         socket.on('gamestarted', (data) => {
           console.log(data.msg)
           this.setState({
-            text: data.text
+            text: data.text,
+            uuid: data.room
+          }, () => {
+            const intervalId = setInterval(this.sendRaceData, 500)
+            this.setState({
+              intervalId
+            })
           })
           console.log(data)
         })
@@ -38,8 +48,22 @@ export default class Game extends React.Component {
         socket.on('gameended', (msg) => {
           console.log('game ended')
           console.log(msg)
+          clearInterval(this.state.intervalId)
+        })
+
+        socket.on('disconnect', () => {
+          clearInterval(this.state.intervalId)
         })
       })
+    })
+  }
+
+  sendRaceData () {
+    socket.emit('racedata', {
+      chars: this.state.chars,
+      room: {
+        uuid: this.state.uuid
+      }
     })
   }
 
@@ -75,7 +99,8 @@ export default class Game extends React.Component {
     if (input === word) {
       this.setState({
         text: splitted.slice(1).join(' '),
-        input: ''
+        input: '',
+        chars: (this.state.chars + word.length)
       })
     }
   }
