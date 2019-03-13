ad099545bdc9fec9854d8b8e7a5a90ad9dbccc50
Aibek Smagulov <smagulovaybek@gmail.com>
2019-03-13 20:26:14 +0900

    configurations changed

--- full-client/config/Config.js
+++ full-client/config/Config.js
@@ -4,7 +4,7 @@ export default {
     GAME_SERVER_API: 'http://192.168.0.102:3000'
   },
   prod: {
-    WEB_SERVER_API: 'http://3.18.147.114:8080',
+    WEB_SERVER_API: 'http://18.188.240.67:8080',
     GAME_SERVER_API: 'http://13.58.228.142:8080'
   }
 }
