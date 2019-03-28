0b797f13f64b9b65ab49f4c1aaa2a6fd07dbc973
Aibek Smagulov <smagulovaybek@gmail.com>
2019-03-28 17:09:32 +0900

    game server pm2 ecosystem file change

--- game-server/ecosystem.config.js
+++ game-server/ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    log_date_format : 'YYYY-MM-DD HH:mm Z',
     env: {
       NODE_ENV: 'dev',
       PORT: '3000'
