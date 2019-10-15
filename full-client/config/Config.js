e3f221c6590910b8e1f500ac83f7b3235e08d352
MadinaB <madinabektayeva@gmail.com>
2019-10-15 21:48:48 +0900

    Design 1st branch commit

--- full-client/config/Config.js
+++ full-client/config/Config.js
@@ -1,11 +1,11 @@
 // TODO(aibek): add https
 export default {
   dev: {
-    WEB_SERVER_API: "http://192.168.200.121:3001",
-    GAME_SERVER_API: "http://192.168.200.121:3000"
+    WEB_SERVER_API: "http://10.64.132.184:3001",
+    GAME_SERVER_API: "http://10.64.132.184:3000"
   },
   prod: {
-    WEB_SERVER_API: "http://192.168.200.121:8080",
-    GAME_SERVER_API: "http://192.168.200.121:8080"
+    WEB_SERVER_API: "http://10.64.132.184:8080",
+    GAME_SERVER_API: "http://10.64.132.184:8080"
   }
 };
